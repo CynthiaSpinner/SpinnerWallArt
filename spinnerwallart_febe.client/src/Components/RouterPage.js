41b71c52d152eaa98cdd74645f5a6be134bb012a
Cynthia <CynthiaSpinner@gmail.com>
2024-05-13 15:34:07 -0500

    finished set up on front end, moving on to display

--- spinnerwallart_febe.client/src/Components/RouterPage.js
+++ spinnerwallart_febe.client/src/Components/RouterPage.js
@@ -5,30 +5,34 @@ import Registration from './Registration';
 import DashBoard from './Users/DashBoard';
 import Orders from './Users/Orders';
 import Profile from './Users/Profile';
-import Cart from './Users/Cart';
+import ProductDisplay from './Users/ProductDisplay';
+import Header from './Users/Header';
+
+import AdminDashBoard from './Admin/AdminDashBoard';
+import AdminHeader from './Admin/AdminHeader';
+import AdminOrder from './Admin/AdminOrders';
+import UserDetails from './Admin/UserDetails';
+
+
+
 export default function RouterPage() {
     return (
         <Router>
             <Routes>
-                <Route path='/' element={<Login />} />
-                <Route path='/' element={<Registration />} />
-                <Route path='/' element={<DashBoard />} />
-                <Route path='/' element={<Orders />} />
-                <Route path='/' element={<Profile />} />
-                <Route path='/' element={<Cart />} />
-                <Route path='/' element={<Login />} />
-                <Route path='/' element={<Login />} />
-                <Route path='/' element={<Login />} />
-                <Route path='/' element={<Login />} />
-                <Route path='/' element={<Login />} />
-                <Route path='/' element={<Login />} />
-                <Route path='/' element={<Login />} />
-                <Route path='/' element={<Login />} />
-                <Route path='/' element={<Login />} />
-                <Route path='/' element={<Login />} />
-                <Route path='/' element={<Login />} />
-                <Route path='/' element={<Login />} />
-                <Route path='/' element={<Login />} />
+                <Route path='/Login' element={<Login />} />
+                <Route path='/registration' element={<Registration />} />
+
+                <Route path='/dashboard' element={<DashBoard />} />        
+                <Route path='/myorders' element={<Orders />} />
+                <Route path='/profile' element={<Profile />} />
+                <Route path='/cart' element={<Cart />} />
+                <Route path='/products' element={<ProductDisplay />} />
+               
+                <Route path='/admindashboard' element={<AdminDashBoard />} />               
+                <Route path='/adminorders' element={<AdminOrders />} />
+                <Route path='/myproducts' element={<AdminProducts />} />
+                <Route path='/customers' element={<UserDetails />} />
+               
             </Routes>
         </Router>
     )
